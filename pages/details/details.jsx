b3f6bce3d31129805e33c816b7c7f35a6d32f584
augustotsilva <augusto.silva@netpos.com.br>
2023-09-02 21:42:52 -0300

    Park images FlatList

--- pages/details/details.jsx
+++ pages/details/details.jsx
@@ -1,177 +1,183 @@
 import {
-  View,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-  Linking,
-  FlatList,
+    View,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    Linking,
+    FlatList,
 } from "react-native";
 
 import CustomText from "../../components/text/text.component";
-import { AntDesign, Foundation, MaterialIcons } from "@expo/vector-icons";
-import MapView, { Marker } from "react-native-maps";
+import {AntDesign, Foundation, MaterialIcons} from "@expo/vector-icons";
+import MapView, {Marker} from "react-native-maps";
 
-import { styles } from "./details.styles";
-import { useEffect, useState } from "react";
+import {styles} from "./details.styles";
+import {useEffect, useState} from "react";
 
-import { data } from "../../data/data.json";
+import {data} from "../../data/data.json";
 
-export default function Details({ navigation, route }) {
-  const [cardDetails, setCardDetails] = useState(null);
-  const [isFavourite, setIsFavourite] = useState(false);
+export default function Details({navigation, route}) {
+    const [cardDetails, setCardDetails] = useState(null);
+    const [isFavourite, setIsFavourite] = useState(false);
 
-  useEffect(() => {
-    data.find((props) => {
-      if (props.id === route.params.id) {
-        setCardDetails({
-          ...props,
+    useEffect(() => {
+        data.find((props) => {
+            if (props.id === route.params.id) {
+                setCardDetails({
+                    ...props,
+                });
+            }
         });
-      }
-    });
-  }, []);
+    }, []);
 
-  const openMap = () => {
-    const scheme = Platform.select({
-      ios: "maps://0,0?q=",
-      android: "geo:0,0?q=",
-    });
-    const latLng = `${cardDetails.coordinates.latitude},${cardDetails.coordinates.longitude}`;
-    const label = cardDetails.name;
-    const url = Platform.select({
-      ios: `${scheme}${label}@${latLng}`,
-      android: `${scheme}${latLng}(${label})`,
-    });
+    const Item = (item) => (
+        <View style={styles.cardContainer}>
+            <Image
+                style={styles.parkImage}
+                source={{uri: item['title'].url}}
+            />
+        </View>
+    );
 
-    Linking.openURL(url);
-  };
 
-  return (
-    <View style={styles.container}>
-      {cardDetails ? (
-        <>
-          <View style={styles.topContainer}>
-            <View style={styles.header}>
-              <TouchableOpacity
-                onPress={() => navigation.navigate("lista")}
-                style={styles.backArrow}
-              >
-                <AntDesign name="arrowleft" size={30} color="white" />
-              </TouchableOpacity>
-              <CustomText style={styles.headerText}>
-                {cardDetails.name}
-              </CustomText>
-              <View>
-                <TouchableOpacity
-                  onPress={() => setIsFavourite(!isFavourite)}
-                  style={styles.backArrow}
-                >
-                  {isFavourite ? (
-                    <AntDesign name="star" size={30} color="#fff" />
-                  ) : (
-                    <AntDesign name="staro" size={30} color="#fff" />
-                  )}
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-          <ScrollView style={styles.mainInfoContainer}>
-            <CustomText style={styles.addressText}>
-              Veja fotos do parque:
-            </CustomText>
-            <View style={styles.cardContainer}>
-              <View>
-                <Image
-                  style={styles.parkImage}
-                  source={{ uri: cardDetails.imageURL }}
-                />
-              </View>
-              <View style={styles.cardContainer}>
-                <Image
-                  style={styles.parkImage}
-                  source={{ uri: cardDetails.imageURL }}
-                />
-              </View>
-            </View>
-            <CustomText style={styles.addressText}>
-              Este parque está localizado em
-            </CustomText>
-            <TouchableOpacity onPress={openMap}>
-              <CustomText style={styles.addressStreet}>
-                {cardDetails.address}{" "}
-                <AntDesign name="arrowright" size={16} color="#579cd1" />
-              </CustomText>
-            </TouchableOpacity>
-            <MapView style={styles.map} region={cardDetails.coordinates}>
-              <Marker coordinate={cardDetails.coordinates} />
-            </MapView>
-            <View style={styles.workingTime}>
-              <AntDesign name="clockcircleo" size={20} color="white" />
-              <CustomText
-                style={{ fontSize: 18, flexShrink: 1, color: "#fff" }}
-              >
-                O parque funciona das {cardDetails.workingTime}
-              </CustomText>
-            </View>
-            <View style={styles.valueContainer}>
-              <CustomText style={styles.value}>Valor</CustomText>
-              <View
-                style={
-                  cardDetails.price === "Gratuito" ? styles.free : styles.paid
-                }
-              >
-                <TouchableOpacity
-                  onPress={() => {
-                    if (cardDetails.priceLink) {
-                      Linking.openURL(cardDetails.priceLink);
-                    } else {
-                      return;
-                    }
-                  }}
-                >
-                  <CustomText style={{ color: "#fff" }}>
-                    {cardDetails.price}
-                  </CustomText>
-                </TouchableOpacity>
-              </View>
-            </View>
-            <View style={styles.contact}>
-              <CustomText>Contato</CustomText>
-              <View style={styles.contactIconContainer}>
-                <TouchableOpacity
-                  onPress={() =>
-                    Linking.openURL(`tel:${cardDetails.telephone}`)
-                  }
-                >
-                  <Foundation name="telephone" size={30} color="#007918" />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => Linking.openURL(`mailto:${cardDetails.email}`)}
-                >
-                  <MaterialIcons name="email" size={30} color="#4d4d4d" />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => Linking.openURL(cardDetails.facebook)}
-                >
-                  <AntDesign name="facebook-square" size={30} color="#0B8AF0" />
-                </TouchableOpacity>
-              </View>
-            </View>
-            <View style={styles.websiteContainer}>
-              <TouchableOpacity
-                style={styles.websiteBtn}
-                onPress={() => Linking.openURL(cardDetails.websiteURL)}
-              >
-                <CustomText style={styles.websiteBtnText}>
-                  Acessar o site
-                </CustomText>
-                <AntDesign name="arrowright" size={16} color="#fff" />
-              </TouchableOpacity>
-            </View>
-          </ScrollView>
-        </>
-      ) : (
-        <></>
-      )}
-    </View>
-  );
+    const openMap = () => {
+        const scheme = Platform.select({
+            ios: "maps://0,0?q=",
+            android: "geo:0,0?q=",
+        });
+        const latLng = `${cardDetails.coordinates.latitude},${cardDetails.coordinates.longitude}`;
+        const label = cardDetails.name;
+        const url = Platform.select({
+            ios: `${scheme}${label}@${latLng}`,
+            android: `${scheme}${latLng}(${label})`,
+        });
+
+        Linking.openURL(url);
+    };
+
+    return (
+        <View style={styles.container}>
+            {cardDetails ? (
+                <>
+                    <View style={styles.topContainer}>
+                        <View style={styles.header}>
+                            <TouchableOpacity
+                                onPress={() => navigation.navigate("lista")}
+                                style={styles.backArrow}
+                            >
+                                <AntDesign name="arrowleft" size={30} color="white"/>
+                            </TouchableOpacity>
+                            <CustomText style={styles.headerText}>
+                                {cardDetails.name}
+                            </CustomText>
+                            <View>
+                                <TouchableOpacity
+                                    onPress={() => setIsFavourite(!isFavourite)}
+                                    style={styles.backArrow}
+                                >
+                                    {isFavourite ? (
+                                        <AntDesign name="star" size={30} color="#fff"/>
+                                    ) : (
+                                        <AntDesign name="staro" size={30} color="#fff"/>
+                                    )}
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                    <ScrollView style={styles.mainInfoContainer}>
+                        <CustomText style={styles.addressText}>
+                            Veja fotos do parque:
+                        </CustomText>
+                        <View>
+                            <FlatList
+                                horizontal
+                                data={cardDetails.pics}
+                                renderItem={({item}) =>
+                                    <Item title={item}/>
+                                }
+                                keyExtractor={item => item.id}
+                            />
+                        </View>
+                        <CustomText style={styles.addressText}>
+                            Este parque está localizado em
+                        </CustomText>
+                        <TouchableOpacity onPress={openMap}>
+                            <CustomText style={styles.addressStreet}>
+                                {cardDetails.address}{" "}
+                                <AntDesign name="arrowright" size={16} color="#579cd1"/>
+                            </CustomText>
+                        </TouchableOpacity>
+                        <MapView style={styles.map} region={cardDetails.coordinates}>
+                            <Marker coordinate={cardDetails.coordinates}/>
+                        </MapView>
+                        <View style={styles.workingTime}>
+                            <AntDesign name="clockcircleo" size={20} color="white"/>
+                            <CustomText
+                                style={{fontSize: 18, flexShrink: 1, color: "#fff"}}
+                            >
+                                O parque funciona das {cardDetails.workingTime}
+                            </CustomText>
+                        </View>
+                        <View style={styles.valueContainer}>
+                            <CustomText style={styles.value}>Valor</CustomText>
+                            <View
+                                style={
+                                    cardDetails.price === "Gratuito" ? styles.free : styles.paid
+                                }
+                            >
+                                <TouchableOpacity
+                                    onPress={() => {
+                                        if (cardDetails.priceLink) {
+                                            Linking.openURL(cardDetails.priceLink);
+                                        } else {
+                                            return;
+                                        }
+                                    }}
+                                >
+                                    <CustomText style={{color: "#fff"}}>
+                                        {cardDetails.price}
+                                    </CustomText>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        <View style={styles.contact}>
+                            <CustomText>Contato</CustomText>
+                            <View style={styles.contactIconContainer}>
+                                <TouchableOpacity
+                                    onPress={() =>
+                                        Linking.openURL(`tel:${cardDetails.telephone}`)
+                                    }
+                                >
+                                    <Foundation name="telephone" size={30} color="#007918"/>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    onPress={() => Linking.openURL(`mailto:${cardDetails.email}`)}
+                                >
+                                    <MaterialIcons name="email" size={30} color="#4d4d4d"/>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    onPress={() => Linking.openURL(cardDetails.facebook)}
+                                >
+                                    <AntDesign name="facebook-square" size={30} color="#0B8AF0"/>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        <View style={styles.websiteContainer}>
+                            <TouchableOpacity
+                                style={styles.websiteBtn}
+                                onPress={() => Linking.openURL(cardDetails.websiteURL)}
+                            >
+                                <CustomText style={styles.websiteBtnText}>
+                                    Acessar o site
+                                </CustomText>
+                                <AntDesign name="arrowright" size={16} color="#fff"/>
+                            </TouchableOpacity>
+                        </View>
+                    </ScrollView>
+                </>
+            ) : (
+                <></>
+            )}
+        </View>
+    );
 }
